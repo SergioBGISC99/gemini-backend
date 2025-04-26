9eb8cdaa959a2a44e4f3b4443576c2a7b8e2ca1c
Sergio Barreras <sergiobg.isc@gmail.com>
2025-04-26 00:22:05 -0700

    Mi primera consulta a Gemini

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { GoogleGenAI } from '@google/genai';
 
 @Injectable()
 export class GeminiService {
-  basicPrompt(dto: BasicPromptDto) {
-    return dto;
+  private ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+  async basicPrompt(dto: BasicPromptDto) {
+    const response = await this.ai.models.generateContent({
+      model: 'gemini-2.0-flash',
+      contents: dto.prompt,
+      config: {
+        systemInstruction:
+          'Responde únicamente en español, en formato makrdown',
+      },
+    });
+
+    return response.text;
   }
 }
