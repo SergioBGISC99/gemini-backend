ef9c0d13f9cd134b0960320d8856cb65fc92988d
Sergio Barreras <sergiobg.isc@gmail.com>
2025-04-25 23:34:38 -0700

    Configuraciones del backend

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('MainLogger');
+
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  
-  await app.listen(3000);
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT);
+
+  logger.log(`Server listening on port ${process.env.PORT}`);
 }
 bootstrap();
